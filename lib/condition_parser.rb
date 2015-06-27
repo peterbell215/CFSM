a3ae4bd4e286be29539501aa46f47c05a9b77685
peter.bell <peter.bell@localhost.localdomain>
2015-06-27 19:53:25 +0100

    Improved the parse tree evaluation functions.

--- lib/condition_parser.rb
+++ lib/condition_parser.rb
@@ -2,10 +2,11 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 require 'parslet'
+require 'byebug'
 
 class ConditionParser < Parslet::Parser
-  root( :or_expression ) 
-    
+  root( :or_expression )
+
   # Simple types
   rule(:digit)          { match["0-9"] }
   rule(:space)          { match["\t "] }
@@ -13,7 +14,7 @@ class ConditionParser < Parslet::Parser
   rule(:string)         { str('"') >> ((str('\"').absent? >> str('"')).absent? >> any).repeat.as(:string) >> str('"') }
   rule(:varname)        { match("[A-Za-z0-9_]").repeat(1) >> ( str(".") >> match("[A-Za-z0-9_]").repeat(1) ).maybe }
   rule(:comparator)     { str("==") | str("!=") | str("<") | str("<=") | str(">") | str(">=") }
-  
+
   # Simple classes
   rule(:number) {
     str('-').maybe >>
@@ -28,7 +29,7 @@ class ConditionParser < Parslet::Parser
   rule(:state_var)      { (str("@") >> varname).as( :state_var ) }
   rule(:state_name)     { (str(":") >> varname).as( :state ) }
   rule(:event)          { varname }
-  
+
   # Grammar parts
   rule(:or_expression)  { ( and_expression >> ( space >> str("or") >> space >> or_expression).repeat(1) ).as(:or) | and_expression }
   rule(:and_expression) { ( evaluation >> ( space >> str("and") >> space >> evaluation).repeat(1) ).as(:and) | evaluation }
@@ -37,46 +38,64 @@ class ConditionParser < Parslet::Parser
   rule(:comparison)     { lhs.as(:left) >> space? >> comparator.as(:comparator) >> space? >> rhs.as(:right) }
   rule(:lhs)            { state_var | event }
   rule(:rhs)            { lhs | number| string | boolean }
-  
-  rule(:boolean_test)   { str("!").maybe >> (state_name | event) }
-  
-  # Compare two arrays within the parse tree. we need to test that for each
-  # element in actual there is a corresponding element in array1.
-  # We have a couple of edge cases to worry about:
-  # [ a, b, b ] != [ a, b ]
-  # [ a, b ] != [ a, b, b] 
-  def self.compare_parse_arrays(array1, actual)
-    array1 = Array.new( array1 )
-    matches = 0
 
-    actual.each do |a|
-      array1.each_index do |e|
-        if compare_parse_trees(a, array1[e] )
-          array1.delete_at( e )
-          matches += 1
-          break;
-        end 
+  rule(:boolean_test)   { str("!").maybe >> (state_name | event) }
+
+  # Compare two arrays within the parse tree and identify the elements that
+  # are common and those that are different.
+  # [ a, b ], [ c, d ] => nil
+  # [ a, b], [a, b ] => true
+  # [ a, b, c ], [ a, b, d ] => { :common => [a, b], only_1 => [:c], only_2 => [:d] }
+  # [ a, b, b ], [ a, b ] => { :common => [a, b], only_1 => nil, only_2 => nil }
+  # [ a, b ] != [ a, b, b] => { :common => [a, b], only_1 => nil, only_2 => nil }
+  def self.compare_parse_arrays(array1, array2)
+    common = []
+    only_1 = Array.new( array1 )
+    only_2 = Array.new( array2 )
+
+    only_1.delete_if do | elem1 |
+      # check if we have already moved elem1 to common array
+      if common.find_index { |elem2| compare_parse_trees( elem1, elem2) }
+        true
+      # check if elem1 is present in only_2 and remove it if necessary
+      elsif only_2.reject! { |elem2| compare_parse_trees( elem1, elem2 ) }
+        # we have already removed elem1 from only_2.  Now need to add to
+        # common and to remove from only_1
+        common << elem1
+        true
       end
     end
-    return matches == actual.length && array1.empty?    
+    
+    # sort out what we return
+    if common.empty?
+      nil
+    elsif only_1.empty? && only_2.empty?
+      true
+    else
+      { :common => common, :only_1 => only_1, :only_2 => only_2 }
+    end
   end
-  
+
+  # compare to elements.
+  def self.compare_parse_elements(elem1, elem2)
+    e1 = elem1.instance_of?( Parslet::Slice ) && elem1.str || elem1
+    e2 = elem2.instance_of?( Parslet::Slice ) && elem2.str || elem2
+
+    e1 == e2
+  end
+
   def self.compare_parse_trees(expected, actual)
-    if actual.instance_of?( Array ) && expected.instance_of?( Array )
-      return compare_parse_arrays(expected, actual)
+     if actual.instance_of?( Array ) && expected.instance_of?( Array )
+      return compare_parse_arrays(expected, actual) == true
     elsif actual.instance_of?( Hash  ) && expected.instance_of?( Hash )
       actual.each_pair do |key, value|
-        if value.instance_of? Parslet::Slice
-          return false if value.str != expected[key]
-        else
-          return false if !compare_parse_trees(expected[key], value)
-        end   
+        return false if !compare_parse_trees(expected[key], value)
       end
-      # if we got here, then every parse element in the actual is also in the
+      # if we got here, then every parse element in the array2 is also in the
       # array1.  Therefore, so long as they are the same length, they are equal.
       return actual.length == expected.length
     else
-      return actual == expected
+      return compare_parse_elements(expected, actual)
     end
   end
 end
--- spec/condition_parser_spec.rb
+++ spec/condition_parser_spec.rb
@@ -6,7 +6,7 @@ require 'condition_parser'
 require 'rspec/expectations'
 
 RSpec::Matchers.define :have_parse_tree do |expected|
-  match { |actual| compare_parse_trees(expected, actual) }
+  match { |actual| ConditionParser::compare_parse_trees(expected, actual) }
 end
 
 describe ConditionParser do
@@ -14,6 +14,45 @@ describe ConditionParser do
     @condition_parser = ConditionParser.new
   end
 
+  describe "#compare_parse_arrays" do
+    it "should return nil for two arrays with no common members" do
+      expect( ConditionParser::compare_parse_arrays( [ :a, :b ], [:c, :d] ) ).to be_nil     
+    end
+ 
+    it "should match two equal arrays" do
+      @array = [ :a, :b ]
+      @result = ConditionParser::compare_parse_arrays( @array, @array.clone )
+      expect( @result ).to be true 
+      
+      @result = ConditionParser::compare_parse_arrays( @array, @array.clone.insert(1, @array.sample ) )
+      expect( @result ).to be true 
+      
+      @result = ConditionParser::compare_parse_arrays( @array.clone.insert(1, @array.sample ), @array )
+       expect( @result ).to be true
+    end
+   
+    it "should not match two un-equal arrays" do
+      @result = ConditionParser::compare_parse_arrays( [ :a, :b ], [:b, :c, :d] )
+      expect( @result[:common] ).to match_array( [ :b ] )
+      expect( @result[:only_1] ).to match_array( [ :a ] )
+      expect( @result[:only_2] ).to match_array( [ :c, :d ] )
+    end
+  end  
+  
+  describe "#compare_parse_trees" do
+    it "should correctly compare the same parse tree." do
+      @res1 = { :or => [
+           { :comparison => { :left=>"a.b", :comparator=>"==", :right=>"1"} },
+           { :comparison => { :left=>"a.c", :comparator=>"<", :right=>"2" } } ] }
+      @res2 = { :or => [
+         { :comparison => { :left=>"a.b", :comparator=>"==", :right=>"1"} },
+         { :comparison => { :left=>"a.c", :comparator=>"<", :right=>"2" } } ] }
+
+      expect( ConditionParser::compare_parse_trees(@res1, @res2) ).to be true
+    end
+  end
+  
+  
   describe "#parse" do
     it "should parse a simple comparison with different white space configs" do
       @result = { :comparison => { :left=>"a.b", :comparator=>"==", :right=>"1" } }
@@ -24,59 +63,30 @@ describe ConditionParser do
     end
 
     it "should parse two comparisons joined by an 'or' expression" do
-      @result = { :or => {
-          :left => {
-            :comparison =>
-              {:left=>"a.b", :comparator=>"==", :right=>"1"}
-          },
-          :right => {
-            :comparison =>
-              { :left=>"a.c", :comparator=>"<", :right=>"2" }
-          }
-        } 
-      }
+      @result = { :or => [
+         { :comparison => { :left=>"a.b", :comparator=>"==", :right=>"1"} },
+         { :comparison => { :left=>"a.c", :comparator=>"<", :right=>"2" } } ] }
 
       expect( @condition_parser.parse( "a.b==1 or a.c<2" ) ).to have_parse_tree( @result )
     end
 
     it "should parse two comparisons joined by an 'and' expression" do
-      @result = { :and => {
-          :left => {
-            :comparison => 
-              { :left=>"a.b", :comparator=>"==", :right=>"1"} 
-          },
-          :right => {
-            :comparison => 
-              {:left=>"a.c", :comparator=>"<", :right=>"2"}
-          }
-        }
-      }
+      @result = { :and => [
+        { :comparison => {:left=>"a.b", :comparator=>"==", :right=>"1"} }, 
+        { :comparison => {:left=>"a.c", :comparator=>"<", :right=>"2" } } ] }
+
       expect( @condition_parser.parse( "a.b==1 and a.c<2" ) ).to have_parse_tree( @result )   
     end
 
     it "should parse a bracketed sub-expression" do
-      @result = { :and => {
-          :left => {
-            :comparison =>
-              { :left=>"a.b", :comparator=>"==", :right=>"1" }
-          },
-          :right => {
-            :brackets => {
-              :or => {
-                :left => {
-                  :comparison => {
-                    :left=>"a.c", :comparator=>">", :right=>"4" }
-                },
-                :right => {
-                  :comparison => {
-                    :left=>"a.c", :comparator=>"<", :right=>"2"
-                  }
-                }
-              }
-            }
+      @result = { :and => [
+        { :comparison => {:left=>"a.b", :comparator=>"==", :right=>"1" } },
+        { :brackets => {
+            :or => [
+              { :comparison => {:left=>"a.c", :comparator=>">", :right=>"4" } },
+              { :comparison => {:left=>"a.c", :comparator=>"<", :right=>"2" } } ]
           }
-        }
-      }
+        } ] }
 
       expect( @condition_parser.parse( "a.b==1 and (a.c>4 or a.c<2)" ) ).to have_parse_tree( @result )
     end
@@ -99,20 +109,6 @@ describe ConditionParser do
     end
   end
   
-  describe "#compare_parse_arrays" do
-    it "should match two equal arrays" do
-      expect( ConditionParser::compare_parse_arrays( [ :a, :b, :c], [:a, :b, :c] ) ).to be true
-    end
-    
-    it "should not match two un-equal arrays" do
-      expect( ConditionParser::compare_parse_arrays( [ :a, :b ], [:b, :c, :d] ) ).to be false
-      expect( ConditionParser::compare_parse_arrays( [:b, :c, :d], [ :a, :b ] ) ).to be false
-      expect( ConditionParser::compare_parse_arrays( [:b, :c, :d], [ :a, :b, :e ] ) ).to be false
-    end
-    
-    it "should not match [ a, b, b ] and [ a, b ]" do
-      expect( ConditionParser::compare_parse_arrays( [ :a, :b, :b ], [:a, :b ] ) ).to be false
-    end
-  end
+  
 end
 
