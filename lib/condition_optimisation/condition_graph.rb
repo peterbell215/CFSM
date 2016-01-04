943a5b7125afd05ef6cda885120dec9f0dc3e6a0
peter.bell215 <peter.bell215@gmail.com>
2016-01-04 07:52:53 +0000

    Added bracket where assignment is done within an elsif statement for tidyness sake.

--- lib/condition_optimisation/condition_graph.rb
+++ lib/condition_optimisation/condition_graph.rb
@@ -217,7 +217,7 @@ module ConditionOptimisation
         if ( starts = START_MATCHER.match(line) )
           # start line is assumed to be the first line.  Therefore, create the graph object.
           graph = ConditionGraph.new( Array.new( input_elements.length-1 ), starts[1].split(', ').map! { |s| s.to_i })
-        elsif elements = LINE_MATCHER.match(line)
+        elsif (elements = LINE_MATCHER.match(line))
           conditions = Set.new elements[2].split(', ').map! { |n| n.to_i }
           transitions = Set.new elements[3].split(', ').map! { |s| s.to_sym }
           edges = elements[4] == 'end' ? nil : Set.new( elements[4].split(', ').map! { |n| n.to_i } )
